1f1b1e6fa938ba7fe832dc3960fb469aad8d0366
betkin <betkin@codsamara.ru>
2022-05-16 20:43:09 +0400

    09-unit-tests

--- homework/09-unit-tests/greeter.go
+++ homework/09-unit-tests/greeter.go
@@ -9,23 +9,27 @@ func Greet(name string, hour int) string {
 	if len(name) == 0 {
 		return "<name>: empty!"
 	}
+	trimmedName := strings.Trim(name, " ")
+	onlyName := strings.Split(trimmedName, " ")[0]
+	if len([]rune(trimmedName)) > 256 {
+		return "<name>: too much!"
+	}
 
 	var greeting string
 	switch {
-	case hour >= 0 && hour < 6:
+	case hour >= 0 && hour <= 6:
 		greeting = "Good night"
-	case hour >= 6 && hour < 12:
+	case hour > 6 && hour <= 12:
 		greeting = "Good morning"
-	case hour >= 12 && hour < 18:
+	case hour > 12 && hour <= 18:
 		greeting = "Hello"
-	case hour >= 18 && hour < 22:
+	case hour > 18 && hour <= 22:
 		greeting = "Good evening"
-	case hour >= 22 && hour < 24:
+	case hour > 22 && hour < 24:
 		greeting = "Good night"
 	default:
 		return "<hour>: range error!"
 	}
 
-	trimmedName := strings.Trim(name, " ")
-	return fmt.Sprintf("%s %s!", greeting, strings.Title(trimmedName))
+	return fmt.Sprintf("%s %s!", greeting, strings.Title(onlyName))
 }
